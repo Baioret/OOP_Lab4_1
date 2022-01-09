ad350aaa9f8dabb229c8950e81d2db917ebc7a20
Baioret <ranreo.reo@gmail.com>
2022-01-09 12:28:54 +0500

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Lab4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sheet = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sheet
+            // 
+            this.sheet.BackColor = System.Drawing.SystemColors.Window;
+            this.sheet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.sheet.Location = new System.Drawing.Point(0, 0);
+            this.sheet.Name = "sheet";
+            this.sheet.Size = new System.Drawing.Size(801, 451);
+            this.sheet.TabIndex = 1;
+            this.sheet.TabStop = false;
+            this.sheet.MouseUp += new System.Windows.Forms.MouseEventHandler(this.sheet_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sheet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox sheet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibraryStorage;
+
+namespace Lab4_1
+{
+    public partial class Form1 : Form
+    {
+        MyStorage storage; // хранилище
+        DrawFigures G; // рисовальщик
+
+        public Form1()
+        {
+            InitializeComponent();
+            storage = new MyStorage();
+            G = new DrawFigures(sheet.Width, sheet.Height);
+        }
+
+        private void sheet_MouseUp(object sender, MouseEventArgs e)
+        {
+            G.UnselectAll(storage);
+            G.DrawStorage(storage);
+
+            //====================
+
+            CCircle obj = new CCircle(e.X, e.Y, G);
+
+            //====================
+
+            obj.DrawCircle();
+            sheet.Image = G.GetBitmap();
+
+            //====================
+
+            storage.add(obj);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            G.DrawStorage(storage);
+        }
+    }
+}
